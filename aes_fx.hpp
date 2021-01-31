5acb22522f46bcd24992bf6ed417643865f399ba
matt <30198937+unmap@users.noreply.github.com>
2021-01-31 10:40:59 -0500

    code cleanup - resharper

--- aes_fx.hpp
+++ aes_fx.hpp
@@ -5,29 +5,33 @@
 #include <vector>
 
 
-inline std::pair <std::vector<std::uint8_t>, int> encrypt ( std::vector<std::uint8_t>& input , std::string_view aes_key_1, std::string_view aes_key_2 )
+__forceinline auto encrypt( std::vector<std::uint8_t>& input , std::string_view aes_key_1 , std::string_view aes_key_2 ) -> std::pair<std::vector<std::uint8_t> , int>
 {
-	std::string raw( input.begin ( ) , input.end ( ) );
-	
-	const int padding = 16 - raw.size ( ) % 16;
-	for ( auto i = 0u; i < padding; ++i )
-		raw += "a";
-	AES_ctx ctx {};
-	AES_init_ctx_iv ( &ctx , reinterpret_cast<const uint8_t*>( aes_key_1.data(  ) ) , reinterpret_cast<const uint8_t*>( aes_key_2.data(  ) ) );
-	AES_CBC_encrypt_buffer ( &ctx , reinterpret_cast<uint8_t*>( raw.data ( ) ) , raw.size ( ) );
+	std::string raw( input.begin( ) , input.end( ) );
 
-	return { std::vector<std::uint8_t>{raw.begin ( ) , raw.end ( )}, padding };
+	const int padding = 16 - raw.size( ) % 16;
+	for( auto i = 0u; i < padding; ++i )
+		raw += "a";
+	AES_ctx ctx { };
+	AES_init_ctx_iv( &ctx , reinterpret_cast<const uint8_t*>( aes_key_1.data( ) ) ,
+							reinterpret_cast<const uint8_t*>( aes_key_2.data( ) ) );
+	
+	AES_CBC_encrypt_buffer( &ctx , reinterpret_cast<uint8_t*>( raw.data( ) ) , raw.size( ) );  // NOLINT(clang-diagnostic-shorten-64-to-32)
+
+	return { std::vector<std::uint8_t> { raw.begin( ) , raw.end( ) } , padding };
 }
 
-__forceinline std::vector<std::uint8_t> decrypt ( std::vector<std::uint8_t> input , std::string_view aes_key_1 , std::string_view aes_key_2,  int paddding )
+__forceinline auto decrypt( std::vector<std::uint8_t> input , std::string_view aes_key_1 , std::string_view aes_key_2 , int paddding ) -> std::vector<std::uint8_t>
 {
-	std::string data ( input.begin ( ) , input.end ( ) );
+	std::string data( input.begin( ) , input.end( ) );
 
-	AES_ctx ctx {};
-	AES_init_ctx_iv ( &ctx , reinterpret_cast<const uint8_t*>( aes_key_1.data(  ) ) , reinterpret_cast<const uint8_t*>( aes_key_2.data(  ) ) );
-	AES_CBC_decrypt_buffer ( &ctx , reinterpret_cast<uint8_t*>( data.data ( ) ) , data.size ( ) );
+	AES_ctx ctx { };
+	AES_init_ctx_iv( &ctx , reinterpret_cast<const uint8_t*>( aes_key_1.data( ) ) ,
+							reinterpret_cast<const uint8_t*>( aes_key_2.data( ) ) );
+	
+	AES_CBC_decrypt_buffer( &ctx , reinterpret_cast<uint8_t*>( data.data( ) ) , data.size( ) );
 
 
-	data.erase ( data.size ( ) - paddding );
-	return std::vector<std::uint8_t>{data.begin ( ) , data.end ( )};
+	data.erase( data.size( ) - paddding );
+	return std::vector<std::uint8_t> { data.begin( ) , data.end( ) };
 }
--- main.cpp
+++ main.cpp
@@ -5,81 +5,80 @@
 #include "aes_fx.hpp"
 
 
-auto generate_header ( const std::vector < std::uint8_t >& in_buffer , int padding ) -> std::string
+auto generate_header( const std::vector<std::uint8_t>& in_buffer , int padding ) -> std::string
 {
 	std::string header;
-	header.append ( "#pragma once\n" );
-	header.append ( "//padding: " );
-	header.append ( std::to_string ( padding ) );
-	header.append ( "\n\n#include <vector>\n" );
-	header.append ( "#include <cstdint>\n\n" );
-	header.append ( "inline std::vector <std::uint8_t> image = \n" );
-	header.append ( "{" );
+	header.append( "#pragma once\n" );
+	header.append( "//padding: " + std::to_string( padding ) + "\n" );
+	header.append( "#include <vector>\n" );
+	header.append( "#include <cstdint>\n\n" );
+	header.append( "inline std::vector <std::uint8_t> image = \n" );
+	header.append( "{" );
 
 	int counter = 0;
-	for ( uint8_t current_char : in_buffer )
+	for( uint8_t current_char : in_buffer )
 	{
-		if ( counter % 25 == 0 )
-			header.append ( "\n    " );
+		if( counter % 25 == 0 )
+			header.append( "\n    " );
 
 		char hex_string [ 10 ];
-		sprintf_s ( hex_string , "0x%02X" , current_char );
+		sprintf_s( hex_string , "0x%02X" , current_char );
 
-		header.append ( hex_string );
+		header.append( hex_string );
 
-		if ( counter != in_buffer.size ( ) - 1 )
-			header.append ( ", " );
+		if( counter != in_buffer.size( ) - 1 )
+			header.append( ", " );
 
 		counter++;
 	}
 
-	header.append ( "\n};\n" );
+	header.append( "\n};\n" );
 	return header;
 }
 
-inline void open_binary_file ( const std::string& file , std::vector<uint8_t>& data )
+inline void open_binary_file( const std::string& file , std::vector<uint8_t>& data )
 {
-	std::ifstream fstr ( file , std::ios::binary );
-	fstr.unsetf ( std::ios::skipws );
-	fstr.seekg ( 0 , std::ios::end );
+	std::ifstream fstr( file , std::ios::binary );
+	fstr.unsetf( std::ios::skipws );
+	fstr.seekg( 0 , std::ios::end );
 
-	const auto file_size = fstr.tellg ( );
+	const auto file_size = fstr.tellg( );
 
-	fstr.seekg ( NULL , std::ios::beg );
-	data.reserve ( static_cast<uint32_t>( file_size ) );
-	data.insert ( data.begin ( ) , std::istream_iterator<uint8_t> ( fstr ) , std::istream_iterator<uint8_t> ( ) );
+	fstr.seekg( NULL , std::ios::beg );
+	data.reserve( static_cast<uint32_t>( file_size ) );
+	data.insert( data.begin( ) , std::istream_iterator<uint8_t>( fstr ) , std::istream_iterator<uint8_t>( ) );
 }
 
-auto main ( int argument_count , char** argument_array ) -> int
+auto main( int argument_count , char** argument_array ) -> int
 {
-	if ( argument_count < 3 )
+	if( argument_count < 3 )
 	{
-		printf ( "usage: simpleaes.exe binary_name.exe" );
+		printf( "usage: simpleaes.exe binary_name.exe" );
 		return -1;
 	}
 
-	std::vector<std::uint8_t>in_buffer {};
-	open_binary_file( argument_array[0] , in_buffer );
+	std::vector<std::uint8_t> in_buffer { };
+	open_binary_file( argument_array [ 0 ] , in_buffer );
 
-	auto [encypted , padding] = encrypt ( in_buffer, argument_array[1], argument_array[2] );
-	
-	const auto enc_buffer_string = generate_header ( encypted , padding );
-	
-	std::ofstream encrypted_file ( std::string ( "output" ) + ".hpp" );
+	auto [ encypted , padding ] = encrypt( in_buffer , argument_array [ 1 ] , argument_array [ 2 ] );
+
+	const auto enc_buffer_string = generate_header( encypted , padding );
+
+	std::ofstream encrypted_file( std::string( "output" ) + ".hpp" );
 	encrypted_file << enc_buffer_string;
-	encrypted_file.close ( );
-	
-	std::cout << "\n =========================================== AES TEST | padding: " << padding << " ===========================================" << std::endl;
+	encrypted_file.close( );
 
-	auto bytes = decrypt ( encypted, argument_array[1] , argument_array[2] , padding );
+	std::cout << "\n =========================================== AES TEST | padding: " << padding <<
+		" ===========================================" << std::endl;
+
+	auto bytes = decrypt( encypted , argument_array [ 1 ] , argument_array [ 2 ] , padding );
 
 	if( bytes != in_buffer )
 	{
 		std::cout << "file mismatch" << std::endl;
 		return 0;
 	}
-	
+
 	std::cout << "\ndone" << std::endl;
-	getchar ( );
-		
+	getchar( );
 }
