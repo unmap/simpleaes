9968854e6a35a4a659552cccca4ad1726fa2ab4d
matt <30198937+unmap@users.noreply.github.com>
2021-02-17 12:30:56 -0500

    cleanup and rename

--- main.cpp
+++ main.cpp
@@ -10,23 +10,22 @@ auto generate_header( const std::vector<std::uint8_t>& in_buffer , int padding )
 	std::string header;
 	header.append( "#pragma once\n" );
 	header.append( "//padding: " + std::to_string( padding ) + "\n" );
-	header.append( "#include <vector>\n" );
 	header.append( "#include <cstdint>\n\n" );
-	header.append( "inline std::vector <std::uint8_t> image = \n" );
+	header.append( "constexpr std::uint8_t image[] = \n" );
 	header.append( "{" );
 
 	int counter = 0;
-	for( uint8_t current_char : in_buffer )
+	for ( uint8_t current_char : in_buffer )
 	{
-		if( counter % 25 == 0 )
+		if ( counter % 25 == 0 )
 			header.append( "\n    " );
 
-		char hex_string [ 10 ];
+		char hex_string[ 10 ];
 		sprintf_s( hex_string , "0x%02X" , current_char );
 
 		header.append( hex_string );
 
-		if( counter != in_buffer.size( ) - 1 )
+		if ( counter != in_buffer.size( ) - 1 )
 			header.append( ", " );
 
 		counter++;
@@ -51,16 +50,16 @@ inline void open_binary_file( const std::string& file , std::vector<uint8_t>& da
 
 auto main( int argument_count , char** argument_array ) -> int
 {
-	if( argument_count < 3 )
+	if ( argument_count < 3 )
 	{
-		printf( "usage: simpleaes.exe binary_name.exe" );
+		printf( "[=] usage: simpleaes.exe binary_name.exe aes_iv aes_key" );
 		return -1;
 	}
-
+	
 	std::vector<std::uint8_t> in_buffer { };
-	open_binary_file( argument_array [ 0 ] , in_buffer );
+	open_binary_file( argument_array[ 0 ] , in_buffer );
 
-	auto [ encypted , padding ] = encrypt( in_buffer , argument_array [ 1 ] , argument_array [ 2 ] );
+	auto [encypted , padding] = encrypt( in_buffer , argument_array[ 1 ] , argument_array[ 2 ] );
 
 	const auto enc_buffer_string = generate_header( encypted , padding );
 
@@ -68,14 +67,13 @@ auto main( int argument_count , char** argument_array ) -> int
 	encrypted_file << enc_buffer_string;
 	encrypted_file.close( );
 
-	std::cout << "\n =========================================== AES TEST | padding: " << padding <<
-		" ===========================================" << std::endl;
+	std::cout << "[+] AES PADDING : " << padding << std::endl;
 
-	auto bytes = decrypt( encypted , argument_array [ 1 ] , argument_array [ 2 ] , padding );
+	const auto bytes = decrypt( encypted , argument_array[ 1 ] , argument_array[ 2 ] , padding );
 
-	if( bytes != in_buffer )
+	if ( bytes != in_buffer )
 	{
-		std::cout << "file mismatch" << std::endl;
+		std::cout << "[-] encrypted file decrypted does not match original" << std::endl;
 		return 0;
 	}
 
